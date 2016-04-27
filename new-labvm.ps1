c0c2344e12e975f6121cdb10b47055db87325b35
KenVB <kenvdb@gmail.com>
2016-04-27 16:36:57 +0200

    continue work in new-labvm. More inline with BP

---
+++ new-labvm.ps1
@@ -0,0 +1,23 @@
+﻿#Requires -RunAsAdministrator
+[CMDLETBinding()]
+
+param(   
+    [Parameter(Mandatory=$true,
+    Valuefrompipeline=$True,
+    Valuefrompipelinebypropertyname=$true
+    )]
+    [string[]]$VMName
+    )
+
+BEGIN{
+$SourcePath="E:\ProgramData\Microsoft\Windows\Hyper-V\Differentialclient\Differntialclient\Virtual Hard Disks\Differntialclient.vhdx"
+}
+PROCESS{
+    foreach($VM in $VMName)
+    {
+        new-vhd -ParentPath $SourcePath -path E:\vms\$VM.vhdx -Differencing
+        New-VM -Name "$VM" -VHDPath "E:\vms\$VM.vhdx" -MemoryStartupBytes 2048MB
+        Start-VM -Name "$VM" 
+    }
+}
+END{}
---
+++ spawn-vm.ps1
@@ -0,0 +1,7 @@
+﻿#Requires -RunAsAdministrator
+1 |
+foreach{
+new-vhd -ParentPath "E:\ProgramData\Microsoft\Windows\Hyper-V\Differentialclient\Differntialclient\Virtual Hard Disks\Differntialclient.vhdx" -path E:\vms\Client$_.vhdx -Differencing
+New-VM -Name "Client$_" -VHDPath "E:\vms\Client$_.vhdx" -MemoryStartupBytes 2048MB
+Start-VM -Name "Client$_" 
+}
