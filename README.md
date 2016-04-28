41b6ac08edd65ec637311411b8d349da9687ea97
KenVB <kenvdb@gmail.com>
2016-04-28 16:19:56 +0200

    Added stuff to readme Added unattended.xml which i use for my sysprepped-machine machines.csv & .xlsx as template for pipeline input many changes to new-labvm.ps1

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 "# spawn-vm" 
+Create new Hyper-V machines, based on a sysprepped, ready VHD to quickly spawn multiple VMs for lab purposes, using differencing disks . 
+Mre info on differencing disks: https://technet.microsoft.com/en-us/library/cc720381(v=ws.10).aspx
+
+Summary:
+- Create a new hyper-v Virtual Machine with a windows client of your choice.
+- Load it up with the settings, software, patches you need.
+- Add an unattend.xml for extra automation. Check my github for mine.
+	Tutorial to make basic unattend.xml: https://support.software.dell.com/k2000-systems-deployment-appliance/kb/154057
+- Run sysprep and shut the machine down
+- Delete the VM from your Hyper-V manager. NOT THE VHDX-file !!!
+- Use this VHDX file as your base for all your new lab VMs. Don't touch it again.
+- Use this script to create new VMs using the differencing VHDX-mechanics.
--- new-labvm.ps1
+++ new-labvm.ps1
@@ -1,4 +1,19 @@
 ﻿#Requires -RunAsAdministrator
+#Requires -Modules Hyper-v
+<#
+.Synopsis
+   Create VMs using differencing VHDs for lab purposes
+.DESCRIPTION
+  Create new Hyper-V machines, based on a sysprepped, ready VHD to quickly spawn multiple VMs for lab purposes.
+.EXAMPLE
+   import-csv "machines.csv" | .\new-labvm.ps1 | start-vm
+   Using the default CSV-template, you create the VMs and inmediatly boot them up.
+.EXAMPLE
+   .\new-labvm.ps1 -VMName <string[]>
+   .\new-labvm.ps1 -VMName bob,jef,lilly
+   Creates 3 VM's, named bob, jef and lilly with default values
+#>
+
 [CMDLETBinding()]
 
 param(   
@@ -11,18 +26,23 @@ param(
     Valuefrompipeline=$true,
     Valuefrompipelinebypropertyname=$true
     )]
-    [string[]]$Sourcepath="E:\vms\Differentialclient\Differntialclient\Virtual Hard Disks\differentialclient.vhdx"
+    [string]$Sourcepath="E:\vms\Differentialclient\Differntialclient\Virtual Hard Disks\Differntialclient.vhdx",
+    [Parameter(Mandatory=$false,
+    Valuefrompipeline=$true,
+    Valuefrompipelinebypropertyname=$true
+    )]
+    [string]$Memory="2048MB"
     )
 
 BEGIN{
 }
 
 PROCESS{
+
     foreach($VM in $VMName)
     {
-        new-vhd -ParentPath $SourcePath -path E:\vms\$VM.vhdx -Differencing
-        New-VM -Name "$VM" -VHDPath "E:\vms\$VM.vhdx" -MemoryStartupBytes 2048MB
-        Start-VM -Name "$VM" 
+    $NewVHD= new-vhd -ParentPath $SourcePath -path E:\vms\$VM.vhdx -Differencing
+    $NewVM = New-VM -Name "$VM" -VHDPath "E:\vms\$VM.vhdx" -MemoryStartupBytes 2048MB
     }
 }
 END{}
